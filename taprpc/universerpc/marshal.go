311e83c26baf98145222c149582992d19cc759eb
ffranr <robyn@lightning.engineering>
2023-08-31 15:22:56 +0100

    taprpc: add marshal functions for universe RPC types

---
+++ taprpc/universerpc/marshal.go
@@ -0,0 +1,45 @@
+package universerpc
+
+import (
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/lightninglabs/taproot-assets/asset"
+)
+
+// MarshalOutpoint marshals a wire.OutPoint into an RPC ready Outpoint.
+//
+// TODO(ffranr): Move this package's Outpoint type and this marshal function to
+// somewhere more general.
+func MarshalOutpoint(outPoint wire.OutPoint) Outpoint {
+	return Outpoint{
+		HashStr: outPoint.Hash.String(),
+		Index:   int32(outPoint.Index),
+	}
+}
+
+// MarshalAssetKey returns an RPC ready AssetKey.
+func MarshalAssetKey(outPoint wire.OutPoint,
+	scriptKeyPubKey *btcec.PublicKey) AssetKey {
+
+	scriptKeyBytes := scriptKeyPubKey.SerializeCompressed()
+	rpcOutPoint := MarshalOutpoint(outPoint)
+
+	return AssetKey{
+		Outpoint: &AssetKey_Op{
+			Op: &rpcOutPoint,
+		},
+		ScriptKey: &AssetKey_ScriptKeyBytes{
+			ScriptKeyBytes: scriptKeyBytes,
+		},
+	}
+}
+
+// MarshalUniverseID returns an RPC ready universe ID.
+func MarshalUniverseID(assetID asset.ID) ID {
+	// TODO(ffranr): Determine whether to use asset ID or group key.
+	return ID{
+		Id: &ID_AssetId{
+			AssetId: assetID[:],
+		},
+	}
+}
